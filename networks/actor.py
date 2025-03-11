b8facf2ff2b14484ee7b98995a653231180fc152
wangyuchen0217 <wangyuchen0217@outlook.com>
2025-03-11 16:56:31 +0900

    commit

--- networks/actor.py
+++ networks/actor.py
@@ -42,3 +42,23 @@ class ActorNetworkPolicy(nn.Module):
         Evaluate the log probabilities for given actions under the current policy.
         """
         return evaluate_lop_pi(self.net(states), self.log_stds, actions)
+    
+    
+class StateDependentPolicy(nn.Module):
+    def __init__(self, state_shape, action_shape, hidden_units=(256, 256),
+                 hidden_activation=nn.ReLU(inplace=True)):
+        super().__init__()
+
+        self.net = build_mlp(
+            input_dim=state_shape[0],
+            output_dim=2 * action_shape[0],
+            hidden_units=hidden_units,
+            hidden_activation=hidden_activation
+        )
+
+    def forward(self, states):
+        return torch.tanh(self.net(states).chunk(2, dim=-1)[0])
+
+    def sample(self, states):
+        means, log_stds = self.net(states).chunk(2, dim=-1)
+        return reparameterize(means, log_stds.clamp_(-20, 2))
--- networks/critic.py
+++ networks/critic.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 from networks.utils import build_mlp
 
@@ -16,3 +17,29 @@ class CriticNetworkPolicy(nn.Module):
 
     def forward(self, states):
         return self.net(states)
+    
+class TwinnedStateActionFunction(nn.Module):
+
+    def __init__(self, state_shape, action_shape, hidden_units=(256, 256),
+                 hidden_activation=nn.ReLU(inplace=True)):
+        super().__init__()
+
+        self.net1 = build_mlp(
+            input_dim=state_shape[0] + action_shape[0],
+            output_dim=1,
+            hidden_units=hidden_units,
+            hidden_activation=hidden_activation
+        )
+        self.net2 = build_mlp(
+            input_dim=state_shape[0] + action_shape[0],
+            output_dim=1,
+            hidden_units=hidden_units,
+            hidden_activation=hidden_activation
+        )
+
+    def forward(self, states, actions):
+        xs = torch.cat([states, actions], dim=-1)
+        return self.net1(xs), self.net2(xs)
+
+    def q1(self, states, actions):
+        return self.net1(torch.cat([states, actions], dim=-1))
