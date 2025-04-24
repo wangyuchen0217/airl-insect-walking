92da1c47ba1c98737aa6e931bd6581c659b68c73
wangyuchen0217 <wangyuchen0217@outlook.com>
2025-04-24 13:39:14 +0900

    commit

--- envs/__init__.py
+++ envs/__init__.py
@@ -3,5 +3,5 @@ from gymnasium.envs.registration import register
 register(
     id='StickInsect-v4',
     entry_point='envs.StickInsectEnv:StickInsectEnv',
-    max_episode_steps=3000,
+    max_episode_steps=1000,
 )
