496a3eedc934d41833e20611de70a4604bb0db70
wangyuchen0217 <wangyuchen0217@outlook.com>
2025-04-29 10:53:00 +0900

    commit

--- test_expert.py
+++ test_expert.py
@@ -10,7 +10,7 @@ from common.base import LoggerWriter
 from common.env import make_env
 
 def main():
-    SAVE_PATH = "/home/yuchen/airl_insect_walking/logs/StickInsect-v4/sac/20250427-1450"
+    SAVE_PATH = "/home/yuchen/airl_insect_walking/logs/StickInsect-v4/sac/20250428-1532"
     ENV_ID = "StickInsect-v4"
     NUM_EPISODES = 10
     SEED = 123
