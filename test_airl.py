68a3e85d76dd42628383d19417332d3ef167eb46
wangyuchen0217 <wangyuchen0217@outlook.com>
2025-04-29 11:10:56 +0900

    commit

--- test_airl.py
+++ test_airl.py
@@ -10,7 +10,7 @@ from common.base import LoggerWriter
 from common.env import make_env
 
 def main():
-    SAVE_PATH = "/home/yuchen/airl_insect_walking/logs/StickInsect-v4/airl/20250428-1542"
+    SAVE_PATH = "/home/yuchen/airl_insect_walking/logs/StickInsect-v4/airl/20250428-1541"
     ENV_ID = "StickInsect-v4"
     NUM_EPISODES = 10
     SEED = 123
