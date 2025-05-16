e2a3cbf5fa44c42a6dddac5259f7df115dfbe6da
wangyuchen0217 <wangyuchen0217@outlook.com>
2025-05-16 11:45:28 +0900

    commit

--- networks/actor.py
+++ networks/actor.py
@@ -18,8 +18,8 @@ class ActorNetworkPolicy(nn.Module):
             hidden_units=hidden_units,
             hidden_activation=hidden_activation
         )
-        # log_stds is a learnable parameter that defines the standard deviation for all actions.
-        self.log_stds = nn.Parameter(torch.zeros(1, action_shape[0]) * 0.5)
+        # log_stds is a learnable parameter that defines the standard deviation for all actions. (torch.zeros(1, action_shape[0]) * 0.5)
+        self.log_stds = nn.Parameter(torch.zeros(1, action_shape[0]))
         self.scale = scale # <--- add scale parameter
 
     def forward(self, states):
