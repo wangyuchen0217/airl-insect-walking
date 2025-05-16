9d10a69534de744db2bcba3ec0a3ff76561d6a55
Yuhen-W <wangyuchen0217@outlook.com>
2025-05-16 17:32:31 +0900

    commit

--- algorithms/ppo.py
+++ algorithms/ppo.py
@@ -45,7 +45,7 @@ class PPO(Algorithm):
             action_shape=action_shape,
             hidden_units=units_actor,
             hidden_activation=nn.Tanh(),
-            scale=1
+            scale=1.8
         ).to(device)
         
         # Critic.
