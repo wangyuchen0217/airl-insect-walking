967f9b42c9d4df7ca5c208c3463bb06d8914fa5f
wangyuchen0217 <wangyuchen0217@outlook.com>
2025-09-10 16:59:59 +0700

    commit

--- test_airl.py
+++ test_airl.py
@@ -10,13 +10,13 @@ from common.normalized_env_new import CoppeliaSimEnv
 import tensorboard
 
 # ======== Parameters (modify these as needed) =========
-ENV_ID = "Medauroidea_60000_offset_wcontact_foot"
+ENV_ID = "Medauroidea_60000_offset"
 ALGO = "airl_logit"
-FILENAME = "20250908-1631" 
+FILENAME = "20250908-1640" 
 PORT = 23000 # CoppeliaSim port: default is 23000
 CUDA = 0
 NUM_EPISODES = 5
-STEP_NUM = 180000  # Choose a certain step number of the saved model or None 
+STEP_NUM =1820000  # Choose a certain step number of the saved model or None 
 LOG = False
 # =================================================
 
